9e553355bd287ac4928972b7c800121a55d6fb93
juancmdev <juancmorenoj@gmail.com>
2024-10-18 19:37:16 -0500

    Notfound page edited

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -6,7 +6,13 @@ const Notfound = () => {
   const error = useRouteError();
   console.log(error);
 
-  return <div>Notfound</div>;
+  return (
+    <div className="notFound">
+      <h1>404</h1>
+      <p>Page not found</p>
+      <p>{error.statusText || error.message}</p>
+    </div>
+  );
 };
 
 export default Notfound;
