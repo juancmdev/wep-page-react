a1ebac9a3dd0cdb8c1010487f84d0b6ba6b28f82
juancmdev <juancmorenoj@gmail.com>
2024-10-20 17:33:20 -0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           className={({ isActive }) =>
             isActive
               ? "text-xs text-yellow-400 transition ease-in-out delay-[0.1s] mb-1"
-              : "text-xs text-black underline mb-1"
+              : "text-xs text-black underline mb-1 hover:text-yellow-400"
           }
           // className="text-xs underline hover:text-yellow-400 cursor-pointer mb-1 tracking-wider"
         >
@@ -36,7 +36,7 @@ const Footer = () => {
         <div className="flex justify-center items-center bg-white w-[25px] h-[25px] hover:bg-yellow-400 cursor-pointer">
           <i class="fa-brands fa-square-github text-3xl"></i>
         </div>
-        <div className="flex justify-center items-center bg-yellow-400 w-[25px] h-[25px] hover:bg-white cursor-pointer">
+        <div className="flex justify-center items-center bg-white w-[25px] h-[25px] hover:bg-yellow-400 cursor-pointer">
           <i class="fa-brands fa-linkedin text-3xl"></i>
         </div>
         <div className="flex justify-center items-center bg-white w-[25px] h-[25px] hover:bg-yellow-400 cursor-pointer">
