3298c007c71b44041451f73d3931035763619c5c
juancmdev <juancmorenoj@gmail.com>
2024-10-20 11:43:25 -0500

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,10 +7,46 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar">
-        <NavLink to="/">Inicio</NavLink>
-        <NavLink to="/productos">Productos</NavLink>
-        <NavLink to="/servicios">Servicios</NavLink>
-        <NavLink to="/contacto">Contacto</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-yellow-400 bg-black transition ease-in-out delay-50w-auto h-auto px-4 py-1.5"
+              : "text-black w-auto h-auto px-4 py-1.5"
+          }
+        >
+          Inicio
+        </NavLink>
+        <NavLink
+          to="/productos"
+          className={({ isActive }) =>
+            isActive
+              ? "text-yellow-400 bg-black transition ease-in-out delay-50w-auto h-auto px-4 py-1.5"
+              : "text-black w-auto h-auto px-4 py-1.5"
+          }
+        >
+          Productos
+        </NavLink>
+        <NavLink
+          to="/servicios"
+          className={({ isActive }) =>
+            isActive
+              ? "text-yellow-400 bg-black transition ease-in-out delay-50 w-auto h-auto px-4 py-1.5"
+              : "text-blackw-auto h-auto px-4 py-1.5"
+          }
+        >
+          Servicios
+        </NavLink>
+        <NavLink
+          to="/contacto"
+          className={({ isActive }) =>
+            isActive
+              ? "text-yellow-400 bg-black transition ease-in-out delay-50 w-auto h-auto px-4 py-1.5"
+              : "text-black w-auto h-auto px-4 py-1.5"
+          }
+        >
+          Contacto
+        </NavLink>
       </nav>
     </>
   );
