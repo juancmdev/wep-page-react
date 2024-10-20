229ac7a2eb990339bd6035fe26d64dae2413d0fb
juancmdev <juancmorenoj@gmail.com>
2024-10-20 10:04:49 -0500

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,16 @@ import "./Header.css";
 const Header = () => {
   return (
     <>
-      <header className="header w-full h-[100px] border-[1px] border-solid border-black flex items-center ">
-        <h1 className="w-28 h-10 text-white bg-black flex justify-center items-center ml-2 tracking-widest">
-          DXS Tienda
+      <header className="header w-full h-[100px] border-[1px] border-solid border-black flex items-center justify-evenly">
+        <h1 className="text-xl w-36 h-16 text-white bg-black flex justify-center items-center ml-2 tracking-[2px]">
+          D<span className="text-yellow-400">X</span>S Ti
+          <span className="text-yellow-400">en</span>da
         </h1>
+        <input
+          type="text"
+          className="w-64 h-10 border-[1px] border-solid  hover:border-yellow-400 cursor-pointer"
+          placeholder="Buscar"
+        />
       </header>
     </>
   );
