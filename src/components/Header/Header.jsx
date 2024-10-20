8ed7ebb9827145b72e108c346ccbd2fa2d9a8cc8
juancmdev <juancmorenoj@gmail.com>
2024-10-20 09:42:40 -0500

    working header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <>
+      <header className="header w-full h-[100px] border-[1px] border-solid border-black flex items-center ">
+        <h1 className="w-28 h-10 text-white bg-black flex justify-center items-center ml-2 tracking-widest">
+          DXS Tienda
+        </h1>
+      </header>
+    </>
+  );
 };
 
 export default Header;
