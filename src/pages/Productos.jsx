62ce148aadcb194738cf1d1c0bd39708f536b7d0
juancmdev <juancmorenoj@gmail.com>
2024-10-22 20:48:48 -0500

    Update Productos.jsx

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -3,18 +3,49 @@ import React from "react";
 const Productos = () => {
   return (
     <>
-      <div className="text-3xl font-bold">
-        <h1 className="text-center my-3">Tienda</h1>
+      <div className="container">
+        <div className="text-3xl font-bold">
+          <h1 className="text-center my-3">Tienda</h1>
+        </div>
+        <div className="grid grid-cols-3">
+          <a href="#" className="flex justify-center items-center">
+            <img
+              src="/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg"
+              alt="Descripción de la imagen"
+              width="200px"
+              height="200px"
+              className="rounded-full mx-2 transition ease-in-out delay-[0.1s] hover:scale-105"
+            />
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <img
+              src="/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg"
+              alt="Descripción de la imagen"
+              width="200px"
+              height="200px"
+              className="rounded-full mx-2 transition ease-in-out delay-[0.1s] hover:scale-105"
+            />
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <img
+              src="/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg"
+              alt="Descripción de la imagen"
+              width="200px"
+              height="200px"
+              className="rounded-full mx-2 transition ease-in-out delay-[0.1s] hover:scale-105"
+            />
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <img
+              src="/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg"
+              alt="Descripción de la imagen"
+              width="200px"
+              height="200px"
+              className="rounded-full mx-2 transition ease-in-out delay-[0.1s] hover:scale-105"
+            />
+          </a>
+        </div>
       </div>
-      <a href="#">
-        <img
-          src="/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg"
-          alt="Descripción de la imagen"
-          width="200px"
-          height="200px"
-          className="rounded-full mx-2 transition ease-in-out delay-[0.1s] hover:scale-105"
-        />
-      </a>
     </>
   );
 };
