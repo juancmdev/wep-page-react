6127ca0e48d435b48fe54fed042622cfa8f3a8ee
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2024-10-24 08:13:52 -0500

    update

---
+++ src/components/Template.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Template = () => {
+  return (
+    <>
+      <div id="template-productos">
+        <li class="lista">
+          <div class="imagen-categoria">
+            <img
+              src="src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg"
+              alt=""
+              width="300px"
+            />
+            <h2>Pelotas de tenis</h2>
+          </div>
+        </li>
+      </div>
+    </>
+  )
+}
+
+export default Template
--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -3,32 +3,16 @@ import React from "react";
 const Productos = () => {
   return (
     <>
-      <div className="container">
+      <div className="">
         <div className="text-3xl font-bold">
           <h1 className="text-center my-3">Tienda</h1>
         </div>
         <div className="grid grid-cols-3">
           <ul id="lista-*******"></ul>
-          <a href="/">
-            <i class="fa-solid fa-arrow-left"></i>
-          </a>
         </div>
       </div>
 
-      <div id="template-productos">
-        <li class="lista">
-          <div class="card">
-            <div class="front">
-              <h2></h2>
-              <img src="src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg" alt="" width="300px"/>
-            </div>
-
-            <div class="back">
-              <h2></h2>
-            </div>
-          </div>
-        </li>
-      </div>
+      
     </>
   );
 };
