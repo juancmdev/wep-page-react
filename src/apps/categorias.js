3e23ad9c18a922ee78fa14de223f54bd28b2f4a9
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2024-10-24 09:00:09 -0500

    updated

---
+++ src/apps/categorias.js
@@ -0,0 +1,23 @@
+// import { useRef } from "react";
+// import Productos from "../pages/Productos";
+// import { categorias } from "../database/database"; 
+
+// const lista = useRef(null);
+// console.log(lista);
+
+
+//const template = document.querySelector("#template-********").content;
+
+//const fragment = document.createDocumentFragment();
+
+/********_routine.forEach((****) => {
+  template.querySelector(".lista .card .front img").src = *****.img;
+  template.querySelector(".lista .card .front img").alt = *****.alt;
+  template.querySelector(".lista .back h2").textContent = *****.back;
+
+  const clone = template.cloneNode(true);
+  fragment.appendChild(clone);
+});*/
+
+//lista.appendChild(fragment);
+
---
+++ src/database/database.js
@@ -0,0 +1,8 @@
+export const categorias = [
+    {
+      img: "/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg",
+      text: "text here",
+      alt: "text here image",
+    },
+  ];
+  
