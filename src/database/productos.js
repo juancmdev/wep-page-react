722390c728ac965a683a1e180b5e99824d4e5237
juancmdev <juancmorenoj@gmail.com>
2024-11-03 09:56:14 -0500

    update

---
+++ src/database/productos.js
@@ -0,0 +1,17 @@
+export const productos = [
+  {
+    id: 1,
+    nombre: "Pelotas de tenis Head",
+    img: "/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg",
+    alt: "text here image",
+    precio: "25.000",
+  },
+
+  {
+    id: 1,
+    nombre: "Pelotas de tenis Head",
+    img: "/src/images/Gemini_Generated_Image_gfzw6igfzw6igfzw.jpg",
+    alt: "text here image",
+    precio: "25.000",
+  },
+];
--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { productos } from "../database/productos.js";
 
 const Tienda = () => {
-  return <></>;
+  const [productosListados, setProductosListados] = useState([]);
+
+  useEffect(() => {
+    setProductosListados(productos);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Tienda</h1>
+      <ul>
+        {productosListados.map((producto) => (
+          <li key={producto.id}>
+            <h2>{producto.nombre}</h2>
+            <img src={producto.img} width={300} />
+            <h3>Precio: $ {producto.precio}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Tienda;
