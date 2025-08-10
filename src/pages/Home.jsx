4e396fc30530b1adec2c0dbb2e810cf075ab0881
juancmdev <juancmorenoj@gmail.com>
2025-08-10 15:46:36 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-
+import { productos } from "../database/productos";
 import "../index.css";
 
 const Home = () => {
   return (
     <>
-      <div className="text-3xl font-bold text-center mt-3"></div>
+      <div className="text-3xl font-bold text-center mt-3">
+        <h1>Bienvenido a nuestra tienda</h1>
+        <img
+          src={productos[0].img}
+          alt=""
+          className="mt-3 mx-auto w-[450px] h-[450px]"
+        />
+      </div>
     </>
   );
 };
