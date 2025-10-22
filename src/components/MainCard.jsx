090eace4b6188cc2428042c7f4ae5a5e94fe9edb
juancmdev <juancmorenoj@gmail.com>
2025-10-21 22:18:58 -0500

    updated

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -9,45 +9,42 @@ const MainCard = () => {
     setProductosListados(productos);
   }, []);
 
-  //   const handleAddToCart = () => {
-  //     // Lógica para añadir al carrito
-  //     setInCart(true);
-  //     console.log(`Producto "${productos.nombre}" añadido al carrito!`);
-
-  //     // Simular un mensaje de confirmación (en lugar de alert)
-  //     setTimeout(() => setInCart(false), 2000);
-  //   };
-
   return (
     <>
-      <div className="w-[300px] h-[450px] bg-yellow-200 rounded shadow-slate-400 shadow-md">
-        <div className="w-[300px] h-[220px] mb-2">
-          <img
-            src={productosListados[0].img}
-            className="object-fit w-full h-full pt-3 pl-3 pr-3 rounded"
-          />
-        </div>
-        <div>
-          <h1 className="text-center text-xl px-2 font-bold">
-            {productosListados[0].nombre}
-          </h1>
-          <p className="text-sm px-3 text-left mb-2">
-            {productosListados[0].descripcion1}
-          </p>
-          <hr className="mx-3" />
-          <div className="flex items-center">
-            <h2 className="mx-4 mt-2 font-extrabold text-3xl">
-              ${productosListados[0].precio}
-            </h2>
-            <h5 className="line-through mt-2 text-xl text-gray-600">
-              ${productosListados[0].antes}
-            </h5>
-          </div>
-          <button className="text-xl h-30 w-64 bg-black text-yellow-500 hover:text-yellow-600  p-2 mt-2 mx-auto block rounded-xl">
-            Comprar Ahora
-          </button>
-        </div>
-      </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-14 gap-5">
+        {productosListados.map((producto) => (
+          <li key={producto.id}>
+            <div className="w-[300px] h-[450px] rounded shadow-slate-400 shadow-md ">
+              <div className="w-[300px] h-[220px] mb-2">
+                <img
+                  src={producto.img}
+                  className="object-fit w-full h-full pt-3 pl-3 pr-3 rounded"
+                />
+              </div>
+              <div>
+                <h1 className="text-center text-xl px-2 font-bold">
+                  {producto.nombre}
+                </h1>
+                <p className="text-sm px-3 text-left mb-2">
+                  {producto.descripcion1}
+                </p>
+                <hr className="mx-3" />
+                <div className="flex items-center">
+                  <h2 className="mx-4 mt-2 font-extrabold text-3xl">
+                    ${producto.precio}
+                  </h2>
+                  <h5 className="line-through mt-2 text-xl text-gray-600">
+                    ${producto.antes}
+                  </h5>
+                </div>
+                <button className="text-xl h-30 w-64 bg-black text-yellow-500 hover:text-yellow-600  p-2 mt-2 mx-auto block rounded-xl">
+                  Comprar Ahora
+                </button>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
